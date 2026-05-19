ee53b8e9272e5fe2b6959157cd8ecab33acabeb0
Sebastien <sebastien.vairet@club-internet.fr>
2026-05-19 22:16:57 +0200

    enable debug

--- app.py
+++ app.py
@@ -452,4 +452,10 @@ def import_json():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+    socketio.run(
+    app,
+    host="0.0.0.0",
+    port=port,
+    debug=True,
+    allow_unsafe_werkzeug=True
+)
